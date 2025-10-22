770c101eeda66c9bbed328b5132395998e875cc8
Dardinger <26DardingerC@mydacc.org>
2025-10-22 12:10:15 -0400

    navabar

--- BPA/post/post.php
+++ BPA/post/post.php
@@ -24,7 +24,42 @@
             header('location: ../landing/landing.php');
         }
     }
+/*
+side bar svgs
+no app logo yet so just put in a black box or smth as a place holder
+user profile svg (this will be used if a user doesn't have a profile picture)
 
+home svg (place holder 1 for the post page)
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+  <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z"/>
+</svg>
+
+dms svg
+
+forum svg
+
+conection svg
+
+courses svg
+
+calendar svg
+
+events svg
+
+settings svg
+
+log out svg
+
+theme toggle
+    sun svg
+
+    moon svg
+
+(create a switch with moon reprenting dark mode on one side and the sun represneting light mode on the other)
+
+top bar
+
+*/
 
 
 ?>
