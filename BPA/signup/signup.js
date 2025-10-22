d643b33b9ff816c905770c6b41f302b810465fb5
Manwaring <26ManwaringS@mydacc.org>
2025-10-22 12:58:15 -0400

    password hiding

--- BPA/signup/signup.js
+++ BPA/signup/signup.js
@@ -1,4 +1,22 @@
-document.getElementById('signupForm').addEventListener('submit', function(e) {
+
+function togglePassword() {
+    var passwordField = document.getElementById("password");
+    const eyeopen = document.getElementById("eye-open");
+    const eyeclose = document.getElementById("eye-slash");
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        eyeopen.classList.remove("hidden");
+        eyeclose.classList.add("hidden");
+    } else {
+        passwordField.type = "password";
+        eyeclose.classList.remove("hidden");
+        eyeopen.classList.add("hidden");
+    }
+}
+
+
+
+/*document.getElementById('signupForm').addEventListener('submit', function(e) {
 e.preventDefault();
 const name = document.getElementById('fullName').value.trim();
 const email = document.getElementById('email').value.trim();
@@ -13,4 +31,4 @@ return;
 
 
 alert(`Welcome, ${name}! Your account has been created.`);
-});
+});*/
