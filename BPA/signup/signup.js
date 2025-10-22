106226a4aefe068ecc1f3ef28ac7cdc44e717bef
Manwaring <26ManwaringS@mydacc.org>
2025-10-22 13:01:49 -0400

    js update

--- BPA/signup/signup.js
+++ BPA/signup/signup.js
@@ -13,22 +13,3 @@ function togglePassword() {
         eyeopen.classList.add("hidden");
     }
 }
-
-
-
-/*document.getElementById('signupForm').addEventListener('submit', function(e) {
-e.preventDefault();
-const name = document.getElementById('fullName').value.trim();
-const email = document.getElementById('email').value.trim();
-const password = document.getElementById('password').value.trim();
-const terms = document.getElementById('terms').checked;
-
-
-if (!name || !email || !password || !terms) {
-alert("Please fill out all fields and accept the terms.");
-return;
-}
-
-
-alert(`Welcome, ${name}! Your account has been created.`);
-});*/
